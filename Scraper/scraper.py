28d14718d0b0ec654fabb461dbef32159082b06b
Deeptanshu-sankhwar <deep.shanky@gmail.com>
2020-11-06 16:53:28 +0530

    formatting-edit

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -10,17 +10,18 @@ soup = BeautifulSoup(html_page.text, 'html.parser')
 
 elements = soup.find_all(attrs = {'class' : 'faculty-col'})
 
-Faculty = {}
+Faculty = []
 
 for element in elements:
-	Faculty[element.find('h3').text] = {
+	Faculty.append({
+		'name': element.find('h3').text,
 		'image': 'https://cse.iith.ac.in'+element.find(attrs={'class' : 'faculty-img'})['src'][2:],
 		'website': element.find(attrs={'class' : 'faculty-link'})['href'],
 		'position': element.find('h4').text,
 		'office': element.find_all('h5')[0].text,
 		'email': element.find_all('h5')[1].text,
 		'domain': element.find_all('h5')[2].text
-	}
+	})
 
 with open("faculty.json", "w") as outfile:  
     json.dump(Faculty, outfile, indent=4) 
