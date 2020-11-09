63eb38d65da5dc61a9c9f4dea0d5754cc6f98954
gshetkar <cs17btech11016@iith.ac.in>
2020-11-09 21:39:06 +0530

    initial kg

---
+++ kg1/kg.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import json
+import networkx as nx
+import matplotlib.pyplot as plt
+
+f = open("../Scraper/faculty.json",)
+dat = json.load(f)
+f.close()
+
+names = []
+position = []
+relation = []
+
+size = len(dat)
+
+
+for i in dat:
+	names.append(i["name"])
+	position.append(i["position"])
+	relation.append("position")
+
+kg1 = pd.DataFrame({'source':names, 'target':position, 'edge':relation})
+
+G = nx.from_pandas_edgelist(kg1,"source","target",edge_attr = True, create_using= nx.MultiDiGraph())
+
+plt.figure(figsize = (40,40))
+pos = nx.spring_layout(G, k = 0.5)
+nx.draw(G, with_labels = True, node_color = 'red', edge_cmap = plt.cm.Blues, pos = pos)
+plt.show()
+print(names[3])
+print(position[0])
+
+
